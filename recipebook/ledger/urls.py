1aa119f74db5ee7c923e2d9739f395a2e8ecc5fa
djpredigua <djpredigua04@gmail.com>
2025-02-27 15:35:32 +0800

    Added models

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.recipe_list, name='home'),
     path('recipes/list/', views.recipe_list, name="recipe_list"),
     path('recipe/1/', views.recipe_1, name="recipe_1"),
     path('recipe/2/', views.recipe_2, name="recipe_2")
-
     ]
 
 app_name = "ledger"
