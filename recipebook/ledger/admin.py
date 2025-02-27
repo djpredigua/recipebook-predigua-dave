56a81479865bfdd19088aaa6d6707d9a9325fe95
djpredigua <djpredigua04@gmail.com>
2025-02-27 18:23:04 +0800

    Lab2 draft
    Modified the recipe_detail.html
    Removed the recipe_1/_2.html
    Modified the urls.py
    Registered models in admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import RecipeIngredient, Recipe
 
 # Register your models here.
+class RecipeGroupAdmin(admin.ModelAdmin):
+    model = Recipe
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
+admin.site.register(Recipe, RecipeGroupAdmin)
+admin.site.register(RecipeIngredient, RecipeAdmin)
--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.recipe_list, name='home'),
-    path('recipes/list/', views.recipe_list, name="recipe_list"),
-    path('recipe/1/', views.recipe_1, name="recipe_1"),
-    path('recipe/2/', views.recipe_2, name="recipe_2")
+    path('', views.RecipeListView.as_view(), name="home"),
+    path('recipes/list/', views.RecipeListView.as_view(), name="recipe_list"),
+    path('recipes/<int:pk>/', views.RecipeDetailView.as_view(), name="recipe_detail"),
     ]
 
 app_name = "ledger"
