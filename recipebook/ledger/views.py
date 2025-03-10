a2fcf448de5419b59ff817f959aaf8649f471ca6
djpredigua <djpredigua04@gmail.com>
2025-03-10 14:55:23 +0800

    updated urls and views

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -5,6 +5,7 @@ from .models import Recipe
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'recipe_list.html'
+    context_object_name = 'recipes'
 
 class RecipeDetailView(DetailView):
     model = Recipe
