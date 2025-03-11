85804e2e3d1c9599498af3330103fd7002e93f48
djpredigua <djpredigua04@gmail.com>
2025-03-11 16:33:14 +0800

    Added base.html, updated the templates, admin, and views

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import RecipeIngredient, Recipe, Ingredient
+from .models import RecipeIngredient, Recipe, Ingredient, Profile
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -9,7 +9,7 @@ class RecipeListView(ListView):
     model = Recipe
     template_name = 'recipe_list.html'
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe_detail.html'
 
