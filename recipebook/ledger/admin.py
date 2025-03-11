c5ede3ce5d0fe56e1b8b293b86119e83939ca040
djpredigua <djpredigua04@gmail.com>
2025-03-11 12:03:16 +0800

    Updated admin.py and models.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,12 +1,36 @@
 from django.contrib import admin
 from .models import RecipeIngredient, Recipe, Ingredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
 class RecipeIngredientLine(admin.TabularInline):
     model = RecipeIngredient
+    extra = 1
 
+# Admin for Recipe
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ("name",)
+    list_display = ("name", "author", "created_on", "updated on")
     inlines = [RecipeIngredientLine]
+    search_fields = ("name", "author_username")
+    list_filter = ("created_on", "updated_on")
+
+# Admin for Ingredient
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+# Profile Inline for User
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "Profiles"
+
+class CustomUserAdmin(BaseUserAdmin):
+    inline = [ProfileInline]
+
 
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
