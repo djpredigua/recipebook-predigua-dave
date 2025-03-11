c41798429d3858fee9ac4e10c9b351a83f0732c2
djpredigua <djpredigua04@gmail.com>
2025-03-11 17:25:55 +0800

    Fixing bugs

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -16,7 +16,7 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
