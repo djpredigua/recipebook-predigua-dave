51875223f35077d9d840cdf1baebc0b6f1d74ebb
djpredigua <djpredigua04@gmail.com>
2025-03-11 17:05:19 +0800

    Updated settings.py and urls.py (project level)
    Still having a problem with templates

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+LOGIN_REDIRECT_URL = "../../recipes/list"
+LOGOUT_REDIRECT_URL = "../../accounts/login"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
