76e16f635b87422838a291c5bb956441f27f3320
djpredigua <djpredigua04@gmail.com>
2025-03-24 21:23:36 +0800

    Update models.py
    Added RecipeImage model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -39,3 +39,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+    
+class RecipeImage(models.Model):
+    recipe_image = models.ImageField(upload_to='recipe_images/static')
+    description = models.CharField(max_length=255)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="images")
+
+    def __str__(self):
+        return f"Image for {self.recipe.name}"
+    
+    def get_absolute_url(self):
+        return reverse("ledger:recipe_detail", args=[self.pk])
