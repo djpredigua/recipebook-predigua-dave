3cf168a034062d0d514195153e2fca121f5e9c0d
djpredigua <djpredigua04@gmail.com>
2025-03-25 17:23:01 +0800

    Updated lab4
    Added html for image and recipe form
    Modified urls and views

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import (
+    RecipeListView,
+    RecipeDetailView,
+    RecipeCreateView,
+    RecipeImageCreateView,
+)
 
 app_name = "ledger"
 
 urlpatterns = [
-    path('recipes/list', RecipeListView.as_view(), name="recipe_list"),
-    path('recipe/<int:pk>/', RecipeDetailView.as_view(), name="recipe_detail"),
+    path("recipes/list", RecipeListView.as_view(), name="recipe_list"),
+    path("recipe/<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
+    path("recipe/add/", RecipeCreateView.as_view(), name="add_recipe"),
+    path("recipe/<int:pk>/add_image", RecipeImageCreateView.as_view, name="add_recipe_image")
     ]
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,15 +1,40 @@
-from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import redirect, get_object_or_404
 
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from .models import Recipe
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
+
 
 class RecipeListView(ListView):
     model = Recipe
-    template_name = 'recipe_list.html'
+    template_name = "recipe_list.html"
 
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
-    template_name = 'recipe_detail.html'
+    template_name = "recipe_detail.html"
 
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = "recipe_form.html"
+    success_url = reverse_lazy("ledger:recipe_list")
+
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    form_class = RecipeForm
+    template_name = "recipe_addImage_form.html"
+
+    def form_invalid(self, form):
+        recipe = get_object_or_404(Recipe, pk=self.kwargs["pk"])
+        form.instance.recipe = recipe
+        return super().form_invalid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy("ledger:recipe_detail", kwargs={"pk": self.kwargs["pk"]})
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["recipe"] = get_object_or_404(Recipe, pk=self.kwargs["pk"])
+        return context 
